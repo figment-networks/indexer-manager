f044216952ade7533b52d66c0dcad59013df6d29
Łukasz Miłkowski <lukanus@uaznia.net>
2020-09-14 16:02:16 +0200

    Add migration version param

--- cmd/manager-migration/main.go
+++ cmd/manager-migration/main.go
@@ -9,19 +9,21 @@ import (
 	"path/filepath"
 
 	"github.com/figment-networks/cosmos-indexer/cmd/manager-migration/config"
-	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 type flags struct {
 	configPath string
+	version    uint
 }
 
 var configFlags = flags{}
 
 func init() {
 	flag.StringVar(&configFlags.configPath, "config", "", "Path to config")
+	flag.UintVar(&configFlags.version, "version", 0, "Version parameter sets db changes to specified revision (up or down)")
 	flag.Parse()
 }
 
@@ -47,8 +49,15 @@ func main() {
 	srcDir := filepath.Join(dir, "migrations")
 	srcPath := fmt.Sprintf("file://%s", srcDir)
 
-	log.Println("using migrations from", srcPath)
-	err = RunMigrations(srcPath, cfg.DatabaseURL)
+	log.Println("Using migrations from: ", srcPath)
+
+	if configFlags.version > 0 {
+		log.Println("Migrating to version: ", configFlags.version)
+		err = MigrateTo(srcPath, cfg.DatabaseURL, configFlags.version)
+	} else {
+		err = RunMigrations(srcPath, cfg.DatabaseURL)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -75,7 +84,7 @@ func initConfig(path string) (config.Config, error) {
 	return *cfg, nil
 }
 
-func RunMigrations(srcPath string, dbURL string) error {
+func RunMigrations(srcPath, dbURL string) error {
 	m, err := migrate.New(srcPath, dbURL)
 	if err != nil {
 		return err
@@ -84,4 +93,15 @@ func RunMigrations(srcPath string, dbURL string) error {
 	defer m.Close()
 
 	return m.Up()
+}
+
+func MigrateTo(srcPath, dbURL string, version uint) error {
+	m, err := migrate.New(srcPath, dbURL)
+	if err != nil {
+		return err
+	}
+
+	defer m.Close()
+
+	return m.Migrate(version)
 }
